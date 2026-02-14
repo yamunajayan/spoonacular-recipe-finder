d76be8e082fd4a7242cbb37e95beb27bd39718a7
yamuna <yamunajayan@gmail.com>
2026-02-14 16:49:12 -0500

    feat: add navigation to recipe detail page from recipe cards

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from "react-router-dom";
+
 type Recipe = {
   id: number;
   title: string;
@@ -5,8 +7,25 @@ type Recipe = {
 };
 
 const RecipeCard = ({ recipe }: { recipe: Recipe }) => {
+  const navigate = useNavigate();
+
+  const slugify = (text: string) =>
+    text
+      .toLowerCase()
+      .replace(/[^\w\s-]/g, "")
+      .trim()
+      .replace(/\s+/g, "-");
+  const handleClick = (id: number, title: string) => {
+    console.log(title);
+    const slug = slugify(title);
+    navigate(`/recipe/${id}/${slug}`);
+  };
   return (
-    <li className="flex flex-col items-center justify-between p-4 bg-white rounded-md shadow h-96 max-w-md">
+    <li
+      className="flex flex-col items-center justify-between p-4 bg-white rounded-md shadow h-96 max-w-md hover:shadow-rose-500 
+    cursor-pointer"
+      onClick={() => handleClick(recipe.id, recipe.title)}
+    >
       <img
         src={recipe.image}
         alt={recipe.title}
