b321fe384aeb3c845c5c00f6b164748898c4302b
yamuna <yamunajayan@gmail.com>
2026-02-13 20:58:34 -0500

    style: initialize global CSS variables and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import "./App.css";
-
 function App() {
   return (
     <>
-      <h1>Medavie Recipe App</h1>
+      <main className="container">
+        <h1>Medavie Recipe App</h1>
+      </main>
     </>
   );
 }
